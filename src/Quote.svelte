2a0b21511039a7e6db03e02a8648ac1a9ab98b65
Aleksandr Gribanov <aleksandr.gribanov@gmail.com>
2019-06-11 15:05:28 +0300

    Add Load button to quotes

--- src/Quote.svelte
+++ src/Quote.svelte
@@ -1,7 +1,11 @@
 <script>
-    let quote = fetch('https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=1')
+    let quote;
+
+    function loadNewQuote(){
+        quote = fetch('https://quotesondesign.com/wp-json/posts?filter%5Borderby%5D=rand&filter%5Bposts_per_page%5D=1')
             .then(res => res.json())
             .then(data => data[0].content);
+    }
 </script>
 
 <style>
@@ -18,3 +22,5 @@
 {:catch}
     Error
 {/await}
+
+<button on:click={loadNewQuote}>Load</button>
